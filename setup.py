1f3b7a73d63e4898378f1892d3dfa23c696a46cd
Jack Gerrits <jack@jackgerrits.com>
2022-03-24 15:58:40 -0400

    update name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="git_changelog_generator",
+    name="git-changelog-generator",
     version="0.1.0",
     py_modules=["git_changelog_generator"],
     entry_points={
         "console_scripts": [
-            "git_changelog_generator=git_changelog_generator:main",
+            "git-changelog-generator=git_changelog_generator:main",
         ],
     },
     python_requires=">=3.6, <4",
